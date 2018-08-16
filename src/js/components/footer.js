f4715c8c08aba410c0f2e922651231cdf3b7284e
Stacy Kvernmo <stacy@oddbird.net>
2018-08-16 17:41:24 -0500

    better style organization WIP

--- src/js/components/footer.js
+++ src/js/components/footer.js
@@ -4,15 +4,14 @@ import * as React from 'react';
 
 const Footer = (props: { logoSrc: string }) => (
   <footer
-    className="slds-global-footer slds-grid
-   slds-grid--align-spread site-contentinfo
-   slds-grid_vertical-align-center spacing-x-large"
+    className="slds-grid slds-grid--align-spread
+    site-contentinfo slds-grid_vertical-align-center spacing-x-large"
   >
+    <div
+      className="slds-global-footer__logo"
+      style={{ backgroundImage: `url(${props.logoSrc})` }}
+    />
     <div className="slds-global-footer__item">
-      <div
-        className="slds-global-footer__logo"
-        style={{ backgroundImage: `url(${props.logoSrc})` }}
-      />
       <p>
         © Copyright 2018 Salesforce.com, inc.&nbsp;
         <a href="https://www.salesforce.com/company/legal/intellectual/">
