735adc1b233631abe5768402c1473a163919e2ff
Jonny Gerig Meyer <jonny@oddbird.net>
2018-08-20 16:17:59 -0400

    Update doc title on route changes.

---
+++ flow-typed/react-document-title.js
@@ -0,0 +1,5 @@
+declare module 'react-document-title' {
+  import typeof { Component } from 'react';
+
+  declare export default Component;
+}
--- src/js/index.js
+++ src/js/index.js
@@ -2,16 +2,18 @@
 
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+import DocumentTitle from 'react-document-title';
 import IconSettings from '@salesforce/design-system-react/components/icon-settings';
 import logger from 'redux-logger';
 import thunk from 'redux-thunk';
 import utilitySprite from '@salesforce-ux/design-system/assets/icons/utility-sprite/svg/symbols.svg';
-import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { combineReducers, createStore, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 
 import Footer from 'components/footer';
+import FourOhFour from 'components/404';
 import Header from 'components/header';
 import getApiFetch from 'utils/api';
 import userReducer from 'accounts/reducer';
@@ -21,29 +23,19 @@ import { doLocalLogout } from 'accounts/actions';
 const MD_logo = require('images/metadeploy-logo.png');
 const SF_logo = require('images/salesforce-logo.png');
 
-const Home = () => (
-  <div>
-    Homepage
-    <Link to="/foo">this is a broken link</Link>
-  </div>
-);
-
-const NoMatch = () => (
-  <div>
-    Oops! 404...
-    <Link to="/">go home</Link>
-  </div>
-);
+const Home = () => <div>Welcome to MetaDeploy!</div>;
 
 const App = () => (
-  <div>
-    <Header logoSrc={MD_logo} />
-    <Switch>
-      <Route exact path="/" component={Home} />
-      <Route component={NoMatch} />
-    </Switch>
-    <Footer logoSrc={SF_logo} />
-  </div>
+  <DocumentTitle title="MetaDeploy">
+    <div>
+      <Header logoSrc={MD_logo} />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route component={FourOhFour} />
+      </Switch>
+      <Footer logoSrc={SF_logo} />
+    </div>
+  </DocumentTitle>
 );
 
 cache.getAll().then(data => {
