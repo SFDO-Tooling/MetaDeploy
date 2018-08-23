7ab8a6c06d863f5312746ca0088c9de740d0ff15
Christian Carter <christian.carter@salesforce.com>
2018-08-23 14:10:53 -0700

    we all know im just poking at this point

--- config/settings/production.py
+++ config/settings/production.py
@@ -3,7 +3,3 @@ from .base import *  # NOQA
 STATICFILES_DIRS = [
     str(PROJECT_ROOT / 'dist/prod'),
 ]
-
-static_dir_root = 'dist/prod'
-
-WHITENOISE_ROOT = PROJECT_ROOT.joinpath(static_dir_root)
