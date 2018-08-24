a65e2dc6b29c229ece4ec3b67e75b1fef898e2cf
Kit La Touche <kit@transneptune.net>
2018-08-24 14:22:01 -0600

    Serialize user into page on initial load

--- metadeploy/api/serializers.py
+++ metadeploy/api/serializers.py
@@ -2,6 +2,20 @@ from rest_framework import serializers
 
 from .models import Project
 
+from django.contrib.auth import get_user_model
+
+
+User = get_user_model()
+
+
+class FullUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = (
+            'username',
+            'email',
+        )
+
 
 class ProjectSerializer(serializers.ModelSerializer):
     class Meta:
