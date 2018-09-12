a4d97232dde7b27e42be64bc5e77b97bc244de71
Jonny Gerig Meyer <jonny@oddbird.net>
2018-09-12 10:46:37 -0400

    Clean up populate_sample_products.py

--- metadeploy/api/management/commands/populate_sample_products.py
+++ metadeploy/api/management/commands/populate_sample_products.py
@@ -6,15 +6,18 @@ from ...models import Product, Version, Plan
 class Command(BaseCommand):
     help = 'Add some sample Products/Versions/Plans to the database.'
 
-    def create_version_and_plan(self, product):
-        version = Version.objects.create(
+    def create_version(self, product):
+        return Version.objects.create(
             product=product,
             label='0.3.1',
             description='This is a description of the product version.',
         )
-        Plan.objects.create(
+
+    def create_plan(self, version, title='Full Install', tier='primary'):
+        return Plan.objects.create(
             version=version,
-            title='Dashboard',
+            title=title,
+            tier=tier,
         )
 
     def handle(self, *args, **options):
@@ -32,36 +35,33 @@ class Command(BaseCommand):
                 ),
             category='salesforce',
         )
-        version1 = Version.objects.create(
-            product=product1,
-            label='0.3.1',
-            description=f'This is the most recent production version.',
-        )
-        Plan.objects.create(
-            version=version1,
-            title='Dashboard',
-        )
-        Plan.objects.create(
-            version=version1,
-            title='Dashboard + Additional Things',
+        version1 = self.create_version(product1)
+        self.create_plan(version1)
+        self.create_plan(
+            version1,
+            title='Reports and Dashboards',
             tier='secondary',
         )
-        Plan.objects.create(
-            version=version1,
-            title='This Other Thing',
+        self.create_plan(
+            version1,
+            title='Account Record Types',
             tier='additional',
         )
-        Plan.objects.create(
-            version=version1,
-            title='Another Additional Thing',
+        self.create_plan(
+            version1,
+            title='Mobile Configuration',
             tier='additional',
         )
+
         product2 = Product.objects.create(
             title=f'Red Salesforce Product',
             description=f'This product should have a red icon.',
             category='salesforce',
             color='#c23934',
         )
+        version2 = self.create_version(product2)
+        self.create_plan(version2)
+
         product3 = Product.objects.create(
             title=f'Custom Icon Salesforce Product',
             description=f'This product should have a custom icon.',
@@ -71,6 +71,9 @@ class Command(BaseCommand):
                     '/avatar3.jpg'
             ),
         )
+        version3 = self.create_version(product3)
+        self.create_plan(version3)
+
         product4 = Product.objects.create(
             title=f'Custom SLDS Icon Salesforce Product',
             description=f'This product should have a custom SLDS icon.',
@@ -78,9 +81,9 @@ class Command(BaseCommand):
             slds_icon_category='utility',
             slds_icon_name='world',
         )
-        self.create_version_and_plan(product2)
-        self.create_version_and_plan(product3)
-        self.create_version_and_plan(product4)
+        version4 = self.create_version(product4)
+        self.create_plan(version4)
+
         for i in range(4):
             product = Product.objects.create(
                 title=f'Sample Community Product {i}',
@@ -97,4 +100,5 @@ class Command(BaseCommand):
                     ),
                 category='community',
             )
-            self.create_version_and_plan(product)
+            version = self.create_version(product)
+            self.create_plan(version)
