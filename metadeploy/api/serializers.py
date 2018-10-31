a946fe4f0d6036b8fdb6a394973d6f95031bc254
Kit La Touche <kit@transneptune.net>
2018-10-31 13:49:10 -0600

    Include 'id' in Job serializer

--- metadeploy/api/serializers.py
+++ metadeploy/api/serializers.py
@@ -112,6 +112,7 @@ class JobSerializer(serializers.ModelSerializer):
     class Meta:
         model = Job
         fields = (
+            'id',
             'user',
             'plan',
             'steps',
