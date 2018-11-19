3cfa62039df8a157a1d4f6c30277444b5770d8cb
Jonny Gerig Meyer <jonny@oddbird.net>
2018-11-19 15:29:51 -0500

    Make expiration default to 1hr in dev.

--- config/settings/local.py
+++ config/settings/local.py
@@ -1,1 +1,11 @@
 from .base import *  # NOQA
+from .base import env
+
+
+# Default token expiration to 1 hr
+TOKEN_LIFETIME_MINUTES = env('TOKEN_LIFETIME_MINUTES', type_=int, default=60)
+PREFLIGHT_LIFETIME_MINUTES = env(
+    'PREFLIGHT_LIFETIME_MINUTES',
+    type_=int,
+    default=60,
+)
