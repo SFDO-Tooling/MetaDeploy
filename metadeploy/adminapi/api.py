66733d063651fc63f5859729a2f58d35a5663acf
Christian Carter <christian.carter@salesforce.com>
2018-12-07 10:57:57 -0800

    allow passing in of route namespace

--- metadeploy/adminapi/api.py
+++ metadeploy/adminapi/api.py
@@ -10,9 +10,8 @@ class AdminAPISerializer(serializers.HyperlinkedModelSerializer):
         fields = "__all__"
 
     def build_url_field(self, field_name, model_class):
-        field_kwargs = {
-            "view_name": f"admin_rest:{self.Meta.model.__name__.lower()}-detail"
-        }  # override default view_name
+        view = f"{self.context['route_ns']}:{self.Meta.model._meta.object_name.lower()}-detail"  # noqa
+        field_kwargs = {"view_name": view}  # override default view_name
 
         return self.serializer_url_field, field_kwargs
 
@@ -23,24 +22,25 @@ class AdminAPISerializer(serializers.HyperlinkedModelSerializer):
         model_field, related_model, to_many, to_field, has_through_model, reverse = (
             relation_info
         )
+        view = f"{self.context['route_ns']}:{related_model._meta.object_name.lower()}-detail"  # noqa
+
         if "view_name" in field_kwargs:
             # we're in a hyperlinkedrelationshipfield, need to fix the view ref...
-            field_kwargs[
-                "view_name"
-            ] = f"admin_rest:{related_model.__name__.lower()}-detail"
+            field_kwargs["view_name"] = view
         return field_class, field_kwargs
 
 
 class AdminAPIViewSet(viewsets.ModelViewSet):
-    app_label = "api"
+    model_app_label = "api"
     model_name = None
     permission_classes = [permissions.IsAdminUser]
     serlializer_base = AdminAPISerializer
+    route_ns = "admin_rest"
 
     @property
     def model(self):
         return apps.get_model(
-            app_label=str(self.app_label), model_name=str(self.model_name)
+            app_label=str(self.model_app_label), model_name=str(self.model_name)
         )
 
     def get_queryset(self):
@@ -50,6 +50,13 @@ class AdminAPIViewSet(viewsets.ModelViewSet):
     def get_serializer_class(self):
         self.serlializer_base.Meta.model = self.model
         return self.serlializer_base
+
+    def get_serializer_context(self,):
+        ctx = super().get_serializer_context()
+        ctx[
+            "route_ns"
+        ] = self.route_ns  # add the route namespace to the serializer context
+        return ctx
 
 
 class ProductViewSet(AdminAPIViewSet):
