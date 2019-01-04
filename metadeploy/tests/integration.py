e259afa8eeab7ad627509183de5084e144b70fac
Kit La Touche <kit@transneptune.net>
2019-01-04 13:43:00 -0700

    📦 Add basic integration tests that touch an SF org

---
+++ metadeploy/tests/integration.py
@@ -0,0 +1,77 @@
+"""
+These are end-to-end integration tests that touch stateful resources,
+like a Salesforce org. They should be run with caution, and needn't be
+run on every test run.
+"""
+
+from os import environ
+
+import pytest
+from django.core.exceptions import ImproperlyConfigured
+
+from metadeploy.api.jobs import JobFlow, run_flows
+from metadeploy.api.models import Job
+
+
+def env(name):
+    try:
+        return environ[name]
+    except KeyError:
+        raise ImproperlyConfigured(
+            f"Cannot run integration tests. Missing environment variable: {name}."
+        )
+
+
+INSTANCE_URL = env("TEST_INSTANCE_URL")
+ORGANIZATION_ID = env("TEST_ORGANIZATION_ID")
+TOKEN = env("TEST_TOKEN")
+TOKEN_SECRET = env("TEST_TOKEN_SECRET")
+
+
+@pytest.mark.integration
+@pytest.mark.django_db
+def test_can_reach_salesforce(
+    social_token_factory,
+    social_account_factory,
+    job_factory,
+    user_factory,
+    plan_factory,
+    step_factory,
+    version_factory,
+    product_factory,
+):
+    user = user_factory(socialaccount_set=[])
+    social_account = social_account_factory(
+        user=user,
+        extra_data={
+            "instance_url": INSTANCE_URL,
+            "organization_details": {
+                "Id": ORGANIZATION_ID,
+                "Name": "Oddbird",
+                "OrganizationType": "Developer Edition",
+            },
+        },
+        socialtoken_set=[],
+    )
+    social_token_factory(account=social_account, token=TOKEN, token_secret=TOKEN_SECRET)
+
+    product = product_factory(repo_url="https://github.com/SFDO-Tooling/CumulusCI-Test")
+    version = version_factory(commit_ish="feature/preflight", product=product)
+    plan = plan_factory(
+        preflight_flow_name="slow_steps_preflight_good",
+        flow_name="slow_steps_flow",
+        version=version,
+    )
+    steps = [step_factory(plan=plan)]
+    job = job_factory(user=user)
+
+    run_flows(
+        user=user,
+        plan=plan,
+        skip_tasks=steps,
+        organization_url=INSTANCE_URL,
+        flow_class=JobFlow,
+        flow_name=plan.flow_name,
+        result_class=Job,
+        result_id=job.id,
+    )
