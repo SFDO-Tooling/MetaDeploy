ecd4e37cbd3b78cc5a103b6adeec7cf63dd77fd5
Kit La Touche <kit@transneptune.net>
2019-01-10 11:01:13 -0700

    🛢 Add help text explaining canceled_at

--- metadeploy/api/models.py
+++ metadeploy/api/models.py
@@ -435,7 +435,13 @@ class Job(HashIdMixin, models.Model):
     org_name = models.CharField(blank=True, max_length=256)
     org_type = models.CharField(blank=True, max_length=256)
     is_public = models.BooleanField(default=False)
-    canceled_at = models.DateTimeField(null=True)
+    canceled_at = models.DateTimeField(
+        null=True,
+        help_text=(
+            "The time at which the Job canceled itself, likely just a bit after it was "
+            "told to cancel itself."
+        ),
+    )
     exception = models.TextField(null=True)
     log = models.TextField(blank=True)
 
@@ -518,7 +524,13 @@ class PreflightResult(models.Model):
     edited_at = models.DateTimeField(auto_now=True)
     is_valid = models.BooleanField(default=True)
     status = models.CharField(choices=Status, max_length=64, default=Status.started)
-    canceled_at = models.DateTimeField(null=True)
+    canceled_at = models.DateTimeField(
+        null=True,
+        help_text=(
+            "The time at which the Job canceled itself, likely just a bit after it was "
+            "told to cancel itself."
+        ),
+    )
     # Maybe we don't use foreign keys here because we want the result to
     # remain static even if steps are subsequently changed:
     results = JSONField(default=dict, blank=True)
