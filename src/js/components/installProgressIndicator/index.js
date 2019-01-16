ed1f8f18290892e59302191253a441cca92bc820
Kit La Touche <kit@transneptune.net>
2019-01-16 15:22:11 -0700

    Try to get progress indiator to show changed state

--- src/js/components/installProgressIndicator/index.js
+++ src/js/components/installProgressIndicator/index.js
@@ -3,32 +3,38 @@
 import * as React from 'react';
 import ProgressIndicator from '@salesforce/design-system-react/components/progress-indicator';
 
+const steps = [
+  {
+    id: 0,
+    label: <i>Log in</i>,
+    assistiveText: "You're logged in.",
+  },
+  {
+    id: 1,
+    label: <i>Start preflight</i>,
+    assistiveText: "You've started the preflight check.",
+  },
+  {
+    id: 2,
+    label: <i>Preflight finished</i>,
+    assistiveText: 'The preflight check has completed.',
+  },
+  {
+    id: 3,
+    label: <i>Start job</i>,
+    assistiveText: "You've started the job.",
+  },
+  {
+    id: 4,
+    label: <i>Job finished</i>,
+    assistiveText: 'The job has completed.',
+  },
+];
+
 const InstallProgressIndicator = (): React.Node => {
   // TODO compute steps situation from the state?
-  const steps = [
-    {
-      id: 0,
-      label: <i>First</i>,
-      assistiveText: 'This is custom text in the assistive text key',
-    },
-    {
-      id: 1,
-      label: <i>Second step</i>,
-      assistiveText: 'This is custom text in the assistive text key',
-    },
-    {
-      id: 2,
-      label: <i>Third step</i>,
-      assistiveText: 'This is custom text in the assistive text key',
-    },
-    {
-      id: 3,
-      label: <i>Another step</i>,
-      assistiveText: 'This is custom text in the assistive text key',
-    },
-  ];
-  const completedSteps = [];
-  const selectedStep = steps[0];
+  const completedSteps = [{ id: 0 }];
+  const selectedStep = { id: 1 };
   return (
     <ProgressIndicator
       steps={steps}
