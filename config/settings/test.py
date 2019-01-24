4823b7d3b205333ff933709f223208b899cff8ff
Christian Carter <christian.carter@salesforce.com>
2019-01-24 10:52:32 -0800

    how bow

--- config/settings/test.py
+++ config/settings/test.py
@@ -1,3 +1,8 @@
 from .base import *  # NOQA
+from .base import env
 
 CHANNEL_LAYERS = {"default": {"BACKEND": "channels.layers.InMemoryChannelLayer"}}
+
+AWS_ACCESS_KEY_ID = env("AWS_ACCESS_KEY_ID", default=None)
+AWS_SECRET_ACCESS_KEY = env("AWS_SECRET_ACCESS_KEY", default=None)
+AWS_STORAGE_BUCKET_NAME = env("AWS_BUCKET_NAME", default=None)
