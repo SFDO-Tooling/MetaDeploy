f62f517e2457ce633ec8b357d77d1a94b3307464
Jonny Gerig Meyer <jonny@oddbird.net>
2019-01-25 12:17:44 -0500

    Temp workaround broken preflights.

--- src/js/components/plans/ctaButton.js
+++ src/js/components/plans/ctaButton.js
@@ -119,7 +119,7 @@ class CtaButton extends React.Component<
         this.autoStartPreflight();
       }
       // Remove query-string from URL
-      history.replace({ search: removeUrlParam(AUTO_START_PREFLIGHT) });
+      history.push({ search: removeUrlParam(AUTO_START_PREFLIGHT) });
     }
   }
 
