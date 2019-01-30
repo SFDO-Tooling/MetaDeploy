c7a190807d598319515c82e2ebfa553b80acf75a
Jonny Gerig Meyer <jonny@oddbird.net>
2019-01-30 18:49:39 -0500

    Add missing flow defs.

--- src/js/utils/i18n.js
+++ src/js/utils/i18n.js
@@ -1,12 +1,11 @@
 // @flow
 
-import i18n from 'i18next';
 import i18n_backend from 'i18next-xhr-backend';
 import i18n_detector from 'i18next-browser-languagedetector';
 import { reactI18nextModule } from 'react-i18next';
+import { use } from 'i18next';
 
-export default i18n
-  .use(i18n_detector)
+export default use(i18n_detector)
   .use(i18n_backend)
   .use(reactI18nextModule)
   .init({
