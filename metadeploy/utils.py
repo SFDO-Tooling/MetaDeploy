aa65d70428c802c37cf5d3a9b6fef6bb619e733e
David Glick <dglick@salesforce.com>
2019-02-01 10:56:58 -0500

    fix header name

--- metadeploy/utils.py
+++ metadeploy/utils.py
@@ -3,7 +3,7 @@ def get_remote_ip(request):
 
     (This may be a proxy, so don't assume it's the client's actual IP address.)
     """
-    value = request.META.get("X_HTTP_FORWARDED_FOR") or request.META.get("REMOTE_ADDR")
+    value = request.META.get("HTTP_X_FORWARDED_FOR") or request.META.get("REMOTE_ADDR")
     # X-Forwarded-For may be a list of multiple IP addresses.
     # The last one was added by Heroku so should be trustworthy.
     return value.split(",")[-1].strip()
