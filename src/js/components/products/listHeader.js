ae92ee4f6cc0f1145ffd8885d5d3d6e6a82f043b
Jonny Gerig Meyer <jonny@oddbird.net>
2019-03-14 11:20:44 -0400

    Remove unnecessary fragment.

--- src/js/components/products/listHeader.js
+++ src/js/components/products/listHeader.js
@@ -5,13 +5,11 @@ import PageHeader from '@salesforce/design-system-react/components/page-header';
 import { t } from 'i18next';
 
 const Header = (): React.Node => (
-  <>
-    <PageHeader
-      className="page-header
+  <PageHeader
+    className="page-header
         slds-p-around_x-large"
-      title={t('Select a Product to Install')}
-    />
-  </>
+    title={t('Select a Product to Install')}
+  />
 );
 
 export default Header;
