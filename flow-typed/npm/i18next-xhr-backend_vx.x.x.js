a7247fb3c515040e6a92411a8ebf021406b866f9
Jonny Gerig Meyer <jonny@oddbird.net>
2019-04-02 16:31:21 -0400

    🐈 Upgrade deps.

--- flow-typed/npm/i18next-xhr-backend_vx.x.x.js
+++ flow-typed/npm/i18next-xhr-backend_vx.x.x.js
@@ -1,5 +1,5 @@
 // flow-typed signature: 2b448bf4c55f3c5371a34f2c887400b1
-// flow-typed version: <<STUB>>/i18next-xhr-backend_v1.5.1/flow_v0.95.1
+// flow-typed version: <<STUB>>/i18next-xhr-backend_v1.5.1/flow_v0.96.0
 
 declare module 'i18next-xhr-backend' {
   declare module.exports: any;
