6edd3784376d2e09bbcf58b60d018c1c5fe2b390
Jonny Gerig Meyer <jonny@oddbird.net>
2019-06-24 16:08:52 -0400

    typo

--- metadeploy/api/paginators.py
+++ metadeploy/api/paginators.py
@@ -5,6 +5,6 @@ class ProductPaginator(PageNumberPagination):
     # We are not currently handling the case of products being
     # created/deleted/reordered between a user fetching one page and another.
     # While the front end gracefully ignores duplicate products, this may result
-    # in a product being missed (until a browser-refetch) if it was added to an
+    # in a product being missed (until a browser-reload) if it was added to an
     # already-fetched page.
     page_size = 25
