778ccbb3963d66f404d4a0dc9eef14f87feffdfc
Jonny Gerig Meyer <jonny@oddbird.net>
2019-07-17 11:27:07 -0400

    🐈 Try webpack upgrade again

--- flow-typed/npm/react-redux_v7.x.x.js
+++ flow-typed/npm/react-redux_v7.x.x.js
@@ -1,5 +1,5 @@
-// flow-typed signature: 15067b7cce771bbc7bb9283c0629c4b2
-// flow-typed version: 72d88e1dff/react-redux_v7.x.x/flow_>=v0.89.x
+// flow-typed signature: 401eaea617572e99de9055824449656e
+// flow-typed version: ffb00c2f5c/react-redux_v7.x.x/flow_>=v0.89.x
 
 /**
 The order of type arguments for connect() is as follows:
@@ -27,6 +27,7 @@ Decrypting the abbreviations:
   RMP = Returned merge props
   CP = Props for returned component
   Com = React Component
+  SS = Selected state
   ST = Static properties of Com
   EFO = Extra factory options (used only in connectAdvanced)
 */
@@ -199,6 +200,19 @@ declare module "react-redux" {
   ): Connector<P, OP, P>;
 
   // ------------------------------------------------------------
+  // Typings for Hooks
+  // ------------------------------------------------------------
+
+  declare export function useDispatch<D>(): D;
+
+  declare export function useSelector<S, SS>(
+    selector: (state: S) => SS,
+    equalityFn?: (a: SS, b: SS) => boolean,
+  ): SS;
+
+  declare export function useStore<Store>(): Store;
+
+  // ------------------------------------------------------------
   // Typings for Provider
   // ------------------------------------------------------------
 
@@ -272,5 +286,8 @@ declare module "react-redux" {
     createProvider: typeof createProvider,
     connect: typeof connect,
     connectAdvanced: typeof connectAdvanced,
+    useDispatch: typeof useDispatch,
+    useSelector: typeof useSelector,
+    useStore: typeof useStore,
   };
 }
