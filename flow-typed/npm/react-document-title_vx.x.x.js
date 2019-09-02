c5a3bc532b9de37c44155561e2714f48f049d0bd
Jonny Gerig Meyer <jonny@oddbird.net>
2019-09-02 16:33:26 -0400

    🐈 Upgrade front-end deps.

--- flow-typed/npm/react-document-title_vx.x.x.js
+++ flow-typed/npm/react-document-title_vx.x.x.js
@@ -1,5 +1,5 @@
 // flow-typed signature: 67975ffb909e1dbe0300551c059ca396
-// flow-typed version: <<STUB>>/react-document-title_v^2.0.3/flow_v0.106.2
+// flow-typed version: <<STUB>>/react-document-title_v^2.0.3/flow_v0.106.3
 
 declare module 'react-document-title' {
   import typeof { Component } from 'react';
