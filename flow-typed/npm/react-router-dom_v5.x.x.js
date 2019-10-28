ae8fb8e2b4a808dcb4e13fd6a505bd6c4e9df6b2
Jonny Gerig Meyer <jonny@oddbird.net>
2019-10-28 17:19:47 -0400

    📦 🐈 Combine dependency upgrades.

--- flow-typed/npm/react-router-dom_v5.x.x.js
+++ flow-typed/npm/react-router-dom_v5.x.x.js
@@ -1,5 +1,5 @@
-// flow-typed signature: 649c193ca607ca46f919eb657f05cba1
-// flow-typed version: cb4e8f3aa2/react-router-dom_v5.x.x/flow_>=v0.104.x
+// flow-typed signature: 46c63aab3b1a381b5b31aa59050e835a
+// flow-typed version: a027b02c08/react-router-dom_v5.x.x/flow_>=v0.104.x
 
 declare module "react-router-dom" {
   declare export var BrowserRouter: React$ComponentType<{|
@@ -177,6 +177,11 @@ declare module "react-router-dom" {
     options?: MatchPathOptions | string | string[],
     parent?: Match
   ): null | Match;
+
+  declare export function useHistory(): $PropertyType<ContextRouter, 'history'>;
+  declare export function useLocation(): $PropertyType<ContextRouter, 'location'>;
+  declare export function useParams(): $PropertyType<$PropertyType<ContextRouter, 'match'>, 'params'>;
+  declare export function useRouteMatch(path?: string): $PropertyType<ContextRouter, 'match'>;
 
   declare export function generatePath(pattern?: string, params?: { +[string]: mixed, ... }): string;
 }
