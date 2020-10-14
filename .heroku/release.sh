4bb86b13d94d6c66d6fd59d07253b7047e05b535
Brandon Parker <brandon.parker@salesforce.com>
2020-10-14 09:04:24 -0700

    only test when test_on_release is set

--- .heroku/release.sh
+++ .heroku/release.sh
@@ -3,16 +3,21 @@ set -e
 
 python manage.py migrate --noinput
 
-repo=$GITHUB_REPO
-token=$GITHUB_TOKEN
+if [ -n "$test_on_release" ] ;
+then
+     echo "Dispatching heroku-release-phase event."
+     repo=$GITHUB_REPO
+     token=$GITHUB_TOKEN
+     curl -X POST \
 
-curl -X POST \
+          -H "Accept: application/vnd.github.v3+json" \
+          -H "Authorization: Bearer $token" \
 
-     -H "Accept: application/vnd.github.v3+json" \
-     -H "Authorization: Bearer $token" \
+          https://api.github.com/repos/$repo/dispatches \
 
-     https://api.github.com/repos/$repo/dispatches \
-
-     -d '{"event_type":"production-release"}'
+          -d '{"event_type":"heroku-release-phase"}'
+else
+     echo "Environment variable test_on_release not set."
+fi
 
 echo "Done."
