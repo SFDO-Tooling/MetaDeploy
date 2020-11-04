09e2be5f13ebffb75b55a7c3cd556014d42c3267
David Glick <dglick@salesforce.com>
2020-11-04 09:29:28 -0500

    Only show copyright in footer if configured in the SiteProfile

--- src/js/components/footer.tsx
+++ src/js/components/footer.tsx
@@ -1,43 +1,32 @@
-import i18n from 'i18next';
 import * as React from 'react';
 
-const Footer = () => {
-  let copyright = `${i18n.t('Copyright')} ${window.GLOBALS.YEAR}`;
-  if (window.GLOBALS.SITE?.company_name) {
-    copyright = `${copyright} ${window.GLOBALS.SITE.company_name}`;
-  }
-  return (
-    <footer
-      className="slds-grid
-        slds-grid--align-spread
-        slds-grid_vertical-align-center
-        slds-wrap
-        slds-p-horizontal_x-large
-        slds-p-vertical_medium
-        slds-text-body_small
-        site-contentinfo"
-    >
-      {window.GLOBALS.SITE?.show_metadeploy_wordmark ? (
-        <div className="slds-text-heading_large">
-          <span data-logo-bit="start">meta</span>
-          <span data-logo-bit="end">deploy</span>
-        </div>
-      ) : (
-        ''
-      )}
-      <div className="footer-item">
-        {window.GLOBALS.SITE?.copyright_notice ? ( // These messages are pre-cleaned by the API
-          <div
-            dangerouslySetInnerHTML={{
-              __html: window.GLOBALS.SITE.copyright_notice,
-            }}
-          />
-        ) : (
-          <p>{`${copyright}.`}</p>
-        )}
+const Footer = () => (
+  <footer
+    className="slds-grid
+      slds-grid--align-spread
+      slds-grid_vertical-align-center
+      slds-wrap
+      slds-p-horizontal_x-large
+      slds-p-vertical_medium
+      slds-text-body_small
+      site-contentinfo"
+  >
+    {window.GLOBALS.SITE?.show_metadeploy_wordmark ? (
+      <div className="slds-text-heading_large">
+        <span data-logo-bit="start">meta</span>
+        <span data-logo-bit="end">deploy</span>
       </div>
-    </footer>
-  );
-};
+    ) : (
+      ''
+    )}
+    <div className="footer-item">
+      <div
+        dangerouslySetInnerHTML={{
+          __html: window.GLOBALS.SITE.copyright_notice,
+        }}
+      />
+    </div>
+  </footer>
+);
 
 export default Footer;
