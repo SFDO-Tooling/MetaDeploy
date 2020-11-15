792ebc6453cd182ecb339513714c860f0a1f37ec
Jonny Gerig Meyer <jonny@oddbird.net>
2020-11-15 11:51:30 -0500

    typo

--- metadeploy/api/admin.py
+++ metadeploy/api/admin.py
@@ -159,7 +159,7 @@ class PlanTemplateAdmin(MetadeployTranslatableAdmin):
 @admin.register(Plan)
 class PlanAdmin(MetadeployTranslatableAdmin):
     autocomplete_fields = ("version",)
-    list_filter = ("version__product", "tier", "order_key", "is_listed")
+    list_filter = ("version__product", "tier", "is_listed")
     list_editable = ("is_listed", "order_key")
     list_display = (
         "title",
