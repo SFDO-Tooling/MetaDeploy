eb694079bf3b93f73048b5aa08507dcce9f446c4
Brandon Parker <brandon.parker@salesforce.com>
2021-02-12 08:24:39 -0800

    custom 500 error view

--- metadeploy/tests/views.py
+++ metadeploy/tests/views.py
@@ -1,16 +1,18 @@
+import pytest
 from unittest import mock
 
-import pytest
+from allauth.socialaccount.providers.oauth2.client import OAuth2Error
 from django.test import RequestFactory
+from django.contrib.auth.models import AnonymousUser
 from sfdo_template_helpers.oauth2.salesforce.views import SalesforcePermissionsError
 
-from ..views import custom_permission_denied_view
+from ..views import custom_permission_denied_view, custom_500_view
 
 
 @pytest.mark.django_db
 @mock.patch("metadeploy.views.render")
 def test_custom_permission_denied_view__sf_permissions(render):
-    request = RequestFactory()
+    request = RequestFactory().get("path")
     exc = SalesforcePermissionsError("I'm sorry Dave.")
     custom_permission_denied_view(request, exc)
 
@@ -23,11 +25,33 @@ def test_custom_permission_denied_view__sf_permissions(render):
 @pytest.mark.django_db
 @mock.patch("metadeploy.views.render")
 def test_custom_permission_denied_view__unknown_error(render):
-    request = RequestFactory()
+    request = RequestFactory().get("path")
     exc = Exception("I'm sorry Dave.")
     custom_permission_denied_view(request, exc)
 
     assert (
         render.call_args[1]["context"]["JS_CONTEXT"]["error_message"]
         == "An internal error occurred while processing your request."
+    )
+
+
+@pytest.mark.django_db
+@mock.patch("metadeploy.views.render")
+def test_custom_500_view__ip_restricted_error(render):
+    try:
+        # raise this to populate info for
+        # call to sys.exec_info() in the view
+        raise OAuth2Error(
+            'Error retrieving access token: b\'{"error":"invalid_grant","error_description":"ip restricted"}\''
+        )
+    except OAuth2Error:
+        factory = RequestFactory()
+        request = factory.get("/accounts/salesforce/login/callback/")
+        request.user = AnonymousUser()
+        custom_500_view(request)
+
+    expected = render.call_args[1]["context"]["JS_CONTEXT"]["error_message"]
+    assert (
+        "We've detected that your org has ip login recstrictions in place."
+        in render.call_args[1]["context"]["JS_CONTEXT"]["error_message"]
     )
--- metadeploy/views.py
+++ metadeploy/views.py
@@ -1,16 +1,38 @@
+import sys
 from django.shortcuts import render
+
+from allauth.socialaccount.providers.oauth2.client import OAuth2Error
 from sfdo_template_helpers.oauth2.salesforce.views import SalesforcePermissionsError
+
+GENERIC_ERROR_MSG = "An internal error occurred while processing your request."
 
 
 def custom_permission_denied_view(request, exception):
+    message = GENERIC_ERROR_MSG
     if isinstance(exception, SalesforcePermissionsError):
         message = str(exception)
-    else:
-        message = "An internal error occurred while processing your request."
 
     return render(
         request,
         "index.html",
         context={"JS_CONTEXT": {"error_message": message}},
         status=403,
+    )
+
+
+def custom_500_view(request):
+    message = GENERIC_ERROR_MSG
+    error_type, value, traceback = sys.exc_info()
+
+    if "ip restricted" in value.args[0]:
+        message = (
+            "We've detected that your org has ip login recstrictions in place. "
+            "Please whitelist the IP addresses listed in the plan's description."
+        )
+
+    return render(
+        request,
+        "index.html",
+        context={"JS_CONTEXT": {"error_message": message}},
+        status=500,
     )
