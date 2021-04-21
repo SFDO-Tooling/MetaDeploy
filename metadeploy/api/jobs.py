6f8cede5d32ace6cfe5825cf0d941a1e380a60be
David Glick <dglick@salesforce.com>
2021-04-20 23:05:36 -0400

    📦 update cumulusci, update set_service call, fix pytest-cov

--- metadeploy/api/jobs.py
+++ metadeploy/api/jobs.py
@@ -196,7 +196,7 @@ def run_flows(*, plan, skip_steps, result_class, result_id):
                 "client_id": settings.SFDX_CLIENT_ID,
             }
         )
-        ctx.keychain.set_service("connected_app", connected_app, True)
+        ctx.keychain.set_service("connected_app", "metadeploy", connected_app)
 
         steps = [
             step.to_spec(
