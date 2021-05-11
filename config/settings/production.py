f7a86f34e0a37a048b47ba00107a20db02d3bd2a
David Glick <dglick@salesforce.com>
2021-05-11 10:17:48 -0400

    Support redis 6 on Heroku

--- config/settings/production.py
+++ config/settings/production.py
@@ -1,3 +1,5 @@
+import ssl
+
 from .base import *  # NOQA
 from .base import PROJECT_ROOT, TEMPLATES
 
@@ -9,3 +11,21 @@ TEMPLATES[0]["DIRS"] = [
 ]
 
 RQ = {"WORKER_CLASS": "metadeploy.rq_worker.ConnectionClosingHerokuWorker"}
+
+if REDIS_LOCATION.startswith("rediss://"):
+    # Fix Redis errors with Heroku self-signed certificates
+    # See:
+    #   - https://github.com/django/channels_redis/issues/235
+    #   - https://github.com/jazzband/django-redis/issues/353
+
+    CACHES["default"]["OPTIONS"]["CONNECTION_POOL_KWARGS"] = {"ssl_cert_reqs": False}
+
+    ssl_context = ssl.SSLContext()
+    ssl_context.check_hostname = False
+
+    heroku_redis_ssl_host = {
+        "address": REDIS_LOCATION,
+        "ssl": ssl_context,
+    }
+
+    CHANNEL_LAYERS["default"]["CONFIG"] = {"hosts": (heroku_redis_ssl_host,)}
