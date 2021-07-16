fe5293d25e7e88827d6655ed0a19cbe9b4c957fe
dvdherron <dvdherron@gmail.com>
2021-07-16 16:20:35 +0000

    fix component import

--- src/stories/pages/404.stories.tsx
+++ src/stories/pages/404.stories.tsx
@@ -1,14 +1,14 @@
 import { Story } from '@storybook/react/types-6-0';
 import React, { ComponentProps } from 'react';
 
-import FourOhFour from '../../js/components/404';
+import FourOhFour from '@/components/404';
 import { withRedux } from '../decorators';
 
 export default {
   title: 'Pages/404/Example',
   component: FourOhFour,
   decorators: [withRedux()],
-}
+};
 
 const Template: Story<ComponentProps<typeof FourOhFour>> = (args) => (
   <FourOhFour {...args} />
