652da915d05da6d70bc610f16c3a8ba79eab9ff0
dvdherron <dvdherron@gmail.com>
2021-07-16 15:27:37 +0000

    🐈 wip add first component

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,5 +1,42 @@
+/* eslint-env browser */
+/* eslint-disable import/no-duplicates */
+
+import 'sass/app.scss';
+
+import IconSettings from '@salesforce/design-system-react/components/icon-settings';
+import settings from '@salesforce/design-system-react/components/settings';
+import actionSprite from '@salesforce-ux/design-system/assets/icons/action-sprite/svg/symbols.svg';
+import customSprite from '@salesforce-ux/design-system/assets/icons/custom-sprite/svg/symbols.svg';
+import doctypeSprite from '@salesforce-ux/design-system/assets/icons/doctype-sprite/svg/symbols.svg';
+import standardSprite from '@salesforce-ux/design-system/assets/icons/standard-sprite/svg/symbols.svg';
+import utilitySprite from '@salesforce-ux/design-system/assets/icons/utility-sprite/svg/symbols.svg';
+import React from 'react';
+import { MemoryRouter } from 'react-router-dom';
+
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
   viewMode: 'story',
   previewTabs: { 'storybook/docs/panel': { hidden: true } },
 };
+
+window.GLOBALS = {};
+
+// For React-SLDS modal a11y
+// https://react.lightningdesignsystem.com/components/modals/
+settings.setAppElement(document.documentElement);
+
+export const decorators = [
+  (Story) => (
+    <MemoryRouter>
+      <IconSettings
+        actionSprite={actionSprite}
+        customSprite={customSprite}
+        doctypeSprite={doctypeSprite}
+        standardSprite={standardSprite}
+        utilitySprite={utilitySprite}
+      >
+        <Story />
+      </IconSettings>
+    </MemoryRouter>
+  ),
+];
---
+++ src/stories/pages/404.stories.tsx
@@ -0,0 +1,21 @@
+import { Story } from '@storybook/react/types-6-0';
+import React, { ComponentProps } from 'react';
+
+import FourOhFour from '../../js/components/404';
+import { withRedux } from '../decorators';
+
+export default {
+  title: 'Pages/404/Example',
+  component: FourOhFour,
+  decorators: [withRedux()],
+}
+
+const Template: Story<ComponentProps<typeof FourOhFour>> = (args) => (
+  <FourOhFour {...args} />
+);
+
+export const FourOhFourComponent = Template.bind({});
+FourOhFourComponent.argTypes = {
+  message: { control: 'text' },
+};
+FourOhFourComponent.storyName = '404';
