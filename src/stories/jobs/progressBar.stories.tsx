c40a7d492e47ce28ddf8105fb737a00fe3afc575
dvdherron <dvdherron@gmail.com>
2021-09-30 13:55:32 +0000

    add draft state components to docs

--- src/stories/jobs/progressBar.stories.tsx
+++ src/stories/jobs/progressBar.stories.tsx
@@ -4,11 +4,14 @@ import React, { ComponentProps } from 'react';
 import ProgressBar from '@/js/components/jobs/progressBar';
 import { Job } from '@/js/store/jobs/reducer';
 
+import { withRedux } from '../decorators';
+
 import { sampleJob1, sampleJob2, sampleJob3, sampleJob4 } from '../fixtures';
 
 export default {
   title: 'Jobs/ProgressBar/Example',
   component: ProgressBar,
+  decorators: [withRedux({ socket: true })],
 };
 
 const jobs: { [key: string]: Job } = {
