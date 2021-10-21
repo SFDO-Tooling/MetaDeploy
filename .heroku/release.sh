7e7c55cc162ae0be09942685401a683ad15d9a49
Brandon Parker <brandon.parker@salesforce.com>
2021-10-21 09:29:42 -0700

    case insensitive match

--- .heroku/release.sh
+++ .heroku/release.sh
@@ -9,8 +9,9 @@ else
      python manage.py migrate --noinput
 fi
 
-# TODO: RELEASE_TEST_ENABLED should be set to "True" (case insensitive)
-if [ -n "$RELEASE_TEST_ENABLED" ]; then
+# RELEASE_TEST_ENABLED needs to be set to 'True' (case-insensitive).
+LOWER_CASE = $(echo $RELEASE_TEST_ENABLED | tr [:upper:] [:lower:])
+if [ $LOWER_CASE = true ]; then
      echo "Release tests enabled. Scheduling tests now."
      python .heroku/schedule_release_test.py 
 fi
