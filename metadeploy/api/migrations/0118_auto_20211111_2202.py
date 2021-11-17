f83107adf8a00d419dee03b186d35149d4e05992
David Reed <d.reed@salesforce.com>
2021-11-16 18:35:17 -0700

    Blacken migration file

--- metadeploy/api/migrations/0118_auto_20211111_2202.py
+++ metadeploy/api/migrations/0118_auto_20211111_2202.py
@@ -6,18 +6,28 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('api', '0117_auto_20211020_1509'),
+        ("api", "0117_auto_20211020_1509"),
     ]
 
     operations = [
         migrations.AddField(
-            model_name='version',
-            name='publish_date',
+            model_name="version",
+            name="publish_date",
             field=models.DateTimeField(blank=True, null=True),
         ),
         migrations.AlterField(
-            model_name='step',
-            name='kind',
-            field=models.CharField(choices=[('metadata', 'Metadata'), ('onetime', 'One Time Apex'), ('managed', 'Package'), ('data', 'Data'), ('other', 'Other')], default='metadata', max_length=64),
+            model_name="step",
+            name="kind",
+            field=models.CharField(
+                choices=[
+                    ("metadata", "Metadata"),
+                    ("onetime", "One Time Apex"),
+                    ("managed", "Package"),
+                    ("data", "Data"),
+                    ("other", "Other"),
+                ],
+                default="metadata",
+                max_length=64,
+            ),
         ),
     ]
