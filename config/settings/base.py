35a18be97e94d1c3a6b922c7b64f6683f4292d4b
Sneha <snehaheda@gmail.com>
2022-01-12 00:23:39 +0530

    Added allowed host

--- config/settings/base.py
+++ config/settings/base.py
@@ -103,6 +103,8 @@ ALLOWED_HOSTS = [
     "localhost",
     "localhost:8000",
     "localhost:8080",
+    "candyshop.blackthorn.io",
+    "deploy.blackthorn.io",
 ] + [
     el.strip()
     for el in env("DJANGO_ALLOWED_HOSTS", default="", type_=lambda x: x.split(","))
