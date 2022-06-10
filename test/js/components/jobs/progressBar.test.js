b419c5e6fbd97208fec3c22674968a941087cb54
David Ray <dray@salesforce.com>
2022-06-10 21:20:13 +0000

    update tests

--- test/js/components/jobs/progressBar.test.js
+++ test/js/components/jobs/progressBar.test.js
@@ -17,16 +17,15 @@ describe('<ProgressBar />', () => {
       job: defaultJob,
     };
     const opts = { ...defaults, ...options };
-    const { getByText, queryByText, rerender } = render(
-      <ProgressBar job={opts.job} />,
-    );
-    return { getByText, queryByText, rerender };
+    const { getByText, getAllByText, queryByText, queryAllByText, rerender } =
+      render(<ProgressBar job={opts.job} />);
+    return { getByText, getAllByText, queryByText, queryAllByText, rerender };
   };
 
   test('renders progress bar', () => {
-    const { getByText } = setup();
+    const { getAllByText } = setup();
 
-    expect(getByText('25% Complete')).toBeVisible();
+    expect(getAllByText('25% Complete')).not.toBeNull();
   });
 
   describe('step progress', () => {
@@ -37,34 +36,34 @@ describe('<ProgressBar />', () => {
 
     test('shows incremental progress within steps', () => {
       const job = { ...defaultJob };
-      const { getByText, queryByText, rerender } = setup({ job });
+      const { getAllByText, queryAllByText, rerender } = setup({ job });
 
-      expect(getByText('25% Complete')).toBeVisible();
-
-      jest.advanceTimersByTime(10 * 1000);
-
-      expect(getByText('38% Complete')).toBeVisible();
+      expect(getAllByText('25% Complete')).toHaveLength(2);
 
       jest.advanceTimersByTime(10 * 1000);
 
-      expect(getByText('45% Complete')).toBeVisible();
+      expect(getAllByText('38% Complete')).toHaveLength(2);
+
+      jest.advanceTimersByTime(10 * 1000);
+
+      expect(getAllByText('45% Complete')).toHaveLength(2);
 
       job.results['2'] = [{ status: 'ok' }];
       rerenderWithI18n(<ProgressBar job={job} />, rerender);
 
-      expect(getByText('50% Complete')).toBeVisible();
+      expect(getAllByText('50% Complete')).toHaveLength(2);
 
       job.status = 'failed';
       rerenderWithI18n(<ProgressBar job={job} />, rerender);
 
       expect(clearInterval).toHaveBeenCalledTimes(2);
-      expect(queryByText('50% Complete')).toBeNull();
+      expect(queryAllByText('50% Complete')).toHaveLength(0);
     });
   });
 
   describe('complete', () => {
     test('renders complete progress bar', () => {
-      const { getByText } = setup({
+      const { getAllByText } = setup({
         job: {
           ...defaultJob,
           results: {
@@ -77,13 +76,13 @@ describe('<ProgressBar />', () => {
         },
       });
 
-      expect(getByText('100% Complete')).toBeVisible();
+      expect(getAllByText('100% Complete')).toHaveLength(2);
     });
   });
 
   describe('failed', () => {
     test('renders failed progress bar', () => {
-      const { getByText, queryByText } = setup({
+      const { getByText, queryAllByText } = setup({
         job: {
           ...defaultJob,
           results: {
@@ -95,13 +94,13 @@ describe('<ProgressBar />', () => {
       });
 
       expect(getByText('Failed')).toBeVisible();
-      expect(queryByText('100% Complete')).toBeNull();
+      expect(queryAllByText('100% Complete')).toHaveLength(1);
     });
   });
 
   describe('canceled', () => {
     test('renders canceled progress bar', () => {
-      const { getByText, queryByText } = setup({
+      const { getByText, queryAllByText } = setup({
         job: {
           ...defaultJob,
           results: {
@@ -113,7 +112,7 @@ describe('<ProgressBar />', () => {
       });
 
       expect(getByText('Canceled')).toBeVisible();
-      expect(queryByText('100% Complete')).toBeNull();
+      expect(queryAllByText('100% Complete')).toHaveLength(1);
     });
   });
 });
