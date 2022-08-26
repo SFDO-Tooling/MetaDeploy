b0da1afd2c862f90f37469ca32296cccc97088f4
James Estevez <jestevez@salesforce.com>
2022-08-26 08:48:04 -0700

    Remove newrelic-python-agent (#3458)
    * Remove newrelic-python-agent
    
    Why?:
    
    1. The last update broke the app and wasted a dozen person hours on
       three teams.
    2. We're not using it.
    
    We think it's related to newrelic/newrelic-python-agent/issues/584, but
    see (2) above.
    
    * fix linting errors
    
    * remove unused import
    
    Co-authored-by: Brandon Parker <brandon.parker@salesforce.com>

--- manage.py
+++ manage.py
@@ -2,11 +2,10 @@
 import os
 import sys
 
-from newrelic import agent
 
 if __name__ == "__main__":
-    agent.initialize()
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
+    # TODO: Remove this entire block? Seems likely to be cruft
     try:
         from django.core.management import execute_from_command_line
     except ImportError:
@@ -15,11 +14,11 @@ if __name__ == "__main__":
         # exceptions on Python 2.
         try:
             import django  # noqa: F401
-        except ImportError:
+        except ImportError as exc:
             raise ImportError(
                 "Couldn't import Django. Are you sure it's installed and "
                 "available on your PYTHONPATH environment variable? Did you "
                 "forget to activate a virtual environment?"
-            )
+            ) from exc
         raise
     execute_from_command_line(sys.argv)
