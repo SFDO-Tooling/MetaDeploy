1fe058be60e606003855a9f66414ff0ca66af36e
David Ray <dray@salesforce.com>
2022-10-07 21:52:01 +0000

    lint

--- metadeploy/api/views.py
+++ metadeploy/api/views.py
@@ -14,7 +14,6 @@ from django_filters.rest_framework import DjangoFilterBackend
 from drf_spectacular.utils import extend_schema
 from rest_framework import generics, mixins, status, viewsets
 import rest_framework.exceptions as drf_exceptions
-from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.decorators import action
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
