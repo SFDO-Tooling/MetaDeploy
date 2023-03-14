b12e9c5477090c76615ee979c1ad1e70d2cae7f8
David Ray <dray@salesforce.com>
2023-03-14 19:06:16 +0000

    more clean up

--- src/js/components/bootstrap/bootstrapPageData.tsx
+++ src/js/components/bootstrap/bootstrapPageData.tsx
@@ -7,23 +7,23 @@ const BootstrapPageData = () => {
   // Setting language direction
   document.dir = i18n.dir();
   /*
-{
-  "PREFLIGHT_LIFETIME_MINUTES":60,
-  "TOKEN_LIFETIME_MINUTES":60,
-  "SITE":{
-    "name":"Default MetaDeploy Name",
-    "company_name":"Default MetaDeploy Company Name",
-    "welcome_text":"<p>Thank you for visiting MetaDeploy. This is the default Welcome Text.</p>",
-    "master_agreement":"<p>This is the default Master Agreement.</p>",
-    "copyright_notice":"<p>Copyright notice <a href=\"#\">Link Test</a></p>",
-    "show_metadeploy_wordmark":true,
-    "company_logo":null,
-    "favicon":null
-  },
-  "YEAR":2023,
-  "SENTRY_DSN":"",
-  "SCRATCH_ORGS_AVAILABLE":true
-}
+    {
+      "PREFLIGHT_LIFETIME_MINUTES":60,
+      "TOKEN_LIFETIME_MINUTES":60,
+      "SITE":{
+        "name":"Default MetaDeploy Name",
+        "company_name":"Default MetaDeploy Company Name",
+        "welcome_text":"<p>Thank you for visiting MetaDeploy. This is the default Welcome Text.</p>",
+        "master_agreement":"<p>This is the default Master Agreement.</p>",
+        "copyright_notice":"<p>Copyright notice <a href=\"#\">Link Test</a></p>",
+        "show_metadeploy_wordmark":true,
+        "company_logo":null,
+        "favicon":null
+      },
+      "YEAR":2023,
+      "SENTRY_DSN":"",
+      "SCRATCH_ORGS_AVAILABLE":true
+    }
   */
   const GLOBALS = window.GLOBALS;
 
--- test/js/components/BootstrapPageData.test.js
+++ test/js/components/BootstrapPageData.test.js
@@ -1,34 +1,33 @@
 import React from 'react';
 
 import BootstrapPageData from '@/js/components/bootstrap/bootstrapPageData';
-// import Footer from '@/js/components/footer';
-
 
 import { render } from './../utils';
 
 const GLOBALS = {
-  "PREFLIGHT_LIFETIME_MINUTES": 60,
-  "TOKEN_LIFETIME_MINUTES": 60,
-  "SITE": {
-    "name": "Default MetaDeploy Name",
-    "company_name": "Default MetaDeploy Company Name",
-    "welcome_text": "<p>Thank you for visiting MetaDeploy. This is the default Welcome Text.</p>",
-    "master_agreement": "<p>This is the default Master Agreement.</p>",
-    "copyright_notice": "<p>Copyright notice <a href=\"#\">Link Test</a></p>",
-    "show_metadeploy_wordmark": true,
-    "company_logo": null,
-    "favicon": 'http://localhost/images/test.png'
+  PREFLIGHT_LIFETIME_MINUTES: 60,
+  TOKEN_LIFETIME_MINUTES: 60,
+  SITE: {
+    name: 'Default MetaDeploy Name',
+    company_name: 'Default MetaDeploy Company Name',
+    welcome_text:
+      '<p>Thank you for visiting MetaDeploy. This is the default Welcome Text.</p>',
+    master_agreement: '<p>This is the default Master Agreement.</p>',
+    copyright_notice: '<p>Copyright notice <a href="#">Link Test</a></p>',
+    show_metadeploy_wordmark: true,
+    company_logo: null,
+    favicon: 'http://localhost/images/test.png',
   },
-  "YEAR": 2023,
-  "SENTRY_DSN": "",
-  "SCRATCH_ORGS_AVAILABLE": true
-}
+  YEAR: 2023,
+  SENTRY_DSN: '',
+  SCRATCH_ORGS_AVAILABLE: true,
+};
 
 describe('<BootstrapPageData />', () => {
   describe('meta', () => {
     beforeAll(() => {
       window.GLOBALS = GLOBALS;
-      window.SITE_NAME = window.GLOBALS.SITE?.name;
+      window.SITE_NAME = window.GLOBALS.SITE.name;
     });
 
     afterAll(() => {
@@ -37,37 +36,13 @@ describe('<BootstrapPageData />', () => {
 
     test('renders meta', () => {
       render(<BootstrapPageData />);
-      expect(
-        document.querySelector("meta[name='author']").content)
-        .toBe(GLOBALS?.SITE.company_name);
-      expect(
-        document.querySelector("link[rel='shortcut icon']").href)
-        .toBe(GLOBALS?.SITE.favicon);
-      expect(
-        document.querySelector("title").text)
-        .toBe(window.SITE_NAME);
-
+      expect(document.querySelector("meta[name='author']").content).toBe(
+        GLOBALS.SITE.company_name,
+      );
+      expect(document.querySelector("link[rel='shortcut icon']").href).toBe(
+        GLOBALS.SITE.favicon,
+      );
+      expect(document.querySelector('title').text).toBe(window.SITE_NAME);
     });
   });
-
 });
-
-// describe('<Footer />', () => {
-//   describe('wordmark', () => {
-//     beforeAll(() => {
-//       window.GLOBALS.SITE = {
-//         show_metadeploy_wordmark: true,
-//       };
-//     });
-
-//     afterAll(() => {
-//       window.GLOBALS = {};
-//     });
-
-//     test('renders wordmark', () => {
-//       const { getByText, debug } = render(<Footer />);
-//       debug();
-//       expect(getByText('meta')).toBeVisible();
-//     });
-//   });
-// });
