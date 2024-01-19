061328bc041c8b9686b5d3b2a88fdeea05797451
Bharath Chadarajupalli <vchadarajupalli@salesforce.com>
2024-01-19 15:54:45 -0800

    Temp Changes

--- .heroku/release.sh
+++ .heroku/release.sh
@@ -3,7 +3,7 @@ set -e
 
 if [ -n "$CTC_URL" ] ; then
      # Run migration surrounded by CTC start/stop
-     python .heroku/check_change_traffic_control.py
+     # python .heroku/check_change_traffic_control.py
 else
      # Simply run the migration
      python manage.py showmigrations account
