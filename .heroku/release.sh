b033db77e04859c2305d2a92dc96fcb62d29bb55
Bharath Chadarajupalli <vchadarajupalli@salesforce.com>
2024-01-23 13:03:44 -0800

    Apply migrations without fake

--- .heroku/release.sh
+++ .heroku/release.sh
@@ -4,10 +4,10 @@ set -e
 if [ -n "$CTC_URL" ] ; then
      # Run migration surrounded by CTC start/stop
      # python .heroku/check_change_traffic_control.py
-     python manage.py migrate account --fake
-     python manage.py showmigrations account
-     python manage.py migrate account 0002
-     python manage.py migrate account
+     # python manage.py migrate account --fake
+     # python manage.py showmigrations account
+     # python manage.py migrate account 0002
+     # python manage.py migrate account
      python manage.py migrate --noinput 
 else
      # Simply run the migration
